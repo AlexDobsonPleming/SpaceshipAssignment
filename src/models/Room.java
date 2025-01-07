d1a21b282dba26b7cbf38fb7bd1f84314ea36da7
Alex Dobson <alexdobsonpleming@gmail.com>
2025-01-07 10:34:19 +0000

    just the floor and the repeating wall

--- src/models/Room.java
+++ src/models/Room.java
@@ -17,9 +17,6 @@ import tooling.*;
 public class Room {
 
   private Surface floor;
-  private Surface ceiling;
-  private Surface rearWall;
-  private Surface windowWall;
   private RepeatingSurface oppositeWall;
 
   private PointLight roomLight;
@@ -39,27 +36,6 @@ public class Room {
     floor = new Surface(gl, floorWidth, floorDepth, cameraIn, lights, floorTexture, Mat4Transform.translate(new Vec3()));
 
 
-    Mat4 ceilingTranslate = Mat4Transform.translate(new Vec3(0, floorToCeilingHeight, 0));
-    Mat4 ceilingRotate = Mat4Transform.rotateAroundX(180);
-    Mat4 ceilingTransform = Mat4.multiply(ceilingTranslate, ceilingRotate);
-
-    ceiling = new Surface(gl, floorWidth, floorDepth, cameraIn, lights, asphalt, ceilingTransform);
-
-    Texture rearWallDiffuse = textures.get("alexander_diffuse");
-    Texture rearWallSpecular = textures.get("alexander_specular");
-    Mat4 rearWallRotate = Mat4Transform.rotateAroundX(90);
-    Mat4 rearWallRaise = Mat4Transform.translate(new Vec3(0, 0, -1 * floorToCeilingHeight / 2));
-    Mat4 rearWallPushBack = Mat4Transform.translate(new Vec3(0, -1 * floorDepth / 2, 0));
-    Mat4 rearWallTransform = Mat4.multiply(Mat4.multiply(rearWallRotate, rearWallRaise), rearWallPushBack);
-    Material shinyMaterial = new Material(new Vec3(1.0f, 0.5f, 0.31f), new Vec3(1.0f, 0.5f, 0.31f), new Vec3(0.5f, 0.5f, 0.5f), 32.0f);
-    rearWall = new Surface(gl, floorWidth, floorToCeilingHeight, cameraIn, lights, rearWallDiffuse, rearWallSpecular, rearWallTransform, shinyMaterial);
-
-    Texture window = textures.get("window");
-    Mat4 windowWallRotate = Mat4.multiply(Mat4Transform.rotateAroundZ(270), Mat4Transform.rotateAroundY(90));
-    Mat4 windowWallRaise = Mat4Transform.translate(new Vec3(0, 0, -1 * floorToCeilingHeight / 2));
-    Mat4 windowWallPushBack = Mat4Transform.translate(new Vec3(0, -1 * floorWidth / 2, 0));
-    Mat4 windowWallTransform = Mat4.multiply(Mat4.multiply(windowWallRotate, windowWallRaise), windowWallPushBack);
-    windowWall = new Surface(gl, floorDepth, floorToCeilingHeight, cameraIn, lights, window, windowWallTransform);
 
 
     Texture repeating = textures.get("repeating_small");
@@ -79,19 +55,12 @@ public class Room {
   }
 
   public void render(GL3 gl) {
-
     floor.render(gl);
-    ceiling.render(gl);
-    rearWall.render(gl);
-    windowWall.render(gl);
     oppositeWall.render(gl);
   }
 
   public void dispose(GL3 gl) {
     floor.dispose(gl);
-    ceiling.dispose(gl);
-    rearWall.dispose(gl);
-    windowWall.dispose(gl);
     oppositeWall.dispose(gl);
     roomLight.dispose(gl);
   }
